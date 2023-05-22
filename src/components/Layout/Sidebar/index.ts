a71516f8b1204f38169210279ed511f36afbaf20
AdrianSajjan <adriansajjan2001@gmail.com>
2023-05-23 00:27:31 +0530

    Removed redundant files

--- src/components/Layout/Sidebar/index.ts
+++ src/components/Layout/Sidebar/index.ts
@@ -1,3 +1,2 @@
-export { default as FontFamilySidebar } from "./fonts-sidebar";
 export { default as PropertySidebar } from "./properties-sidebar";
 export { default as LayerSidebar } from "./layers-sidebar";
