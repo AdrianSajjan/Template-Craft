ab22e408bd16b46d047ebe8f235485517ec13146
AdrianSajjan <adriansajjan2001@gmail.com>
2023-05-23 00:26:26 +0530

    Refactored component names

--- src/components/Layout/Footer/index.ts
+++ src/components/Layout/Footer/index.ts
@@ -1,1 +1,1 @@
-export { default as Footer } from "./main-footer";
+export { default as Footer } from "./footer";
--- src/components/Layout/Sidebar/index.ts
+++ src/components/Layout/Sidebar/index.ts
@@ -1,3 +1,3 @@
-export { default as FontFamilySidebar } from "./Fonts";
+export { default as FontFamilySidebar } from "./fonts-sidebar";
 export { default as PropertySidebar } from "./properties-sidebar";
 export { default as LayerSidebar } from "./layers-sidebar";
